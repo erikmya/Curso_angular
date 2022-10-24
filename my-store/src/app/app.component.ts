c16c7e76563ac6c428810bf33b47ea220405185d
erikmya <erik.maya@librerialeo.com.mx>
2022-10-24 12:22:56 -0500

    Clase 7 Property Binding

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -10,4 +10,10 @@ export class AppComponent {
   name = 'Erik';
   age = 28;
   img = 'https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png';
+  btnDisabled = true;
+  person = {
+    name: 'Erik',
+    age: '23',
+    avatar: 'https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png'
+  }
 }
