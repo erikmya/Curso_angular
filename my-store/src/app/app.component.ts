930faaf66531d7b3b9e38a1b207d34d46876981e
erikmya <erik.maya@librerialeo.com.mx>
2022-10-25 16:59:28 -0500

    Clase 18 ngClass y ngStyle

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -21,6 +21,11 @@ export class AppComponent {
   }
   names: string[] = ['Erik', 'Juan', 'Isaac'];
   newName = '';
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red',
+  };
   products: Product[] = [
     {
       name: 'EL mejor juguete',
